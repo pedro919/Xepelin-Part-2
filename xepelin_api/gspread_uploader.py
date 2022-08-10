98a169a8b137b43dcf29351c45e1c35c5905a1d9
pirioja <pirioja@uc.cl>
2022-08-10 17:05:44 -0400

    fix: fixed credentials issue

--- xepelin_api/gspread_uploader.py
+++ xepelin_api/gspread_uploader.py
@@ -4,23 +4,14 @@ import os
 from gspread_dataframe import set_with_dataframe
 from loguru import logger
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
 
 def upload_dataframe_to_google_spread_sheet(df):
     logger.info('entering')
-    credentials = { 
-    "type": os.environ.get("GSPREAD_TYPE"),
-    "project_id": os.environ.get("GSPREAD_PROJECT_ID"),
-    "private_key_id" : os.environ.get("GSPREAD_PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("GSPREAD_PRIVATE_KEY").replace("'\\'", "'\'"),
-    "client_email": os.environ.get("GSPREAD_CLIENT_EMAIL"),
-    "client_id": os.environ.get("GSPREAD_CLIENT_ID"),
-    "auth_uri": os.environ.get("GSPREAD_AUTH_URI"),
-    "token_uri": os.environ.get("GSPREAD_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("GSPREAD_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.environ.get("GSPREAD_CLIENT_X509_CERT_URL")}
+    credentials = json.loads(os.environ.get("GCLOUD_CREDENTIALS"))
     logger.info(credentials)
     try:
         gc = gspread.service_account_from_dict(credentials)
@@ -33,4 +24,6 @@ def upload_dataframe_to_google_spread_sheet(df):
         set_with_dataframe(worksheet, df)
     except Exception as e:
         logger.info(e)
-        logger.info('Error')        
+        logger.info('Error') 
+    
+    
