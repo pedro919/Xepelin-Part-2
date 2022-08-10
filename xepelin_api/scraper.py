f9312a3fbc38c24fa3ac9532b75e59b7a5f8c0c4
pirioja <pirioja@uc.cl>
2022-08-10 15:30:08 -0400

    fix: added uploader to blog_scraper

--- xepelin_api/scraper.py
+++ xepelin_api/scraper.py
@@ -1,6 +1,5 @@
 from selenium import webdriver, common
 from selenium.webdriver.common.by import By
-from xepelin_api.gspread_uploader import upload_dataframe_to_google_spread_sheet
 import time
 import pandas as pd
 import os
@@ -34,4 +33,19 @@ def blog_scraper(url):
         posts_list.append(post_dict)
 
     df = pd.DataFrame(posts_list)
-    upload_dataframe_to_google_spread_sheet(df)
+    credentials = { 
+    "type": os.getenv("GSPREAD_TYPE"),
+    "project_id": os.getenv("GSPREAD_PROJECT_ID"),
+    "private_key_id" : os.getenv("GSPREAD_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GSPREAD_PRIVATE_KEY"),
+    "client_email": os.getenv("GSPREAD_CLIENT_EMAIL"),
+    "client_id": os.getenv("GSPREAD_CLIENT_ID"),
+    "auth_uri": os.getenv("GSPREAD_AUTH_URI"),
+    "token_uri": os.getenv("GSPREAD_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GSPREAD_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GSPREAD_CLIENT_X509_CERT_URL")}
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open_by_key(os.getenv("SPREADSHEET_KEY"))
+    worksheet = sh.get_worksheet(0)
+    worksheet.clear()
+    set_with_dataframe(worksheet, df)
