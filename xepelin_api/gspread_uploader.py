2445cc52b3f282d0c1ae3aea9014a0637374c377
pirioja <pirioja@uc.cl>
2022-08-10 17:09:36 -0400

    fix: Deleted logs

--- xepelin_api/gspread_uploader.py
+++ xepelin_api/gspread_uploader.py
@@ -2,28 +2,14 @@
 import gspread
 import os
 from gspread_dataframe import set_with_dataframe
-from loguru import logger
-from dotenv import load_dotenv
 import json
-
-load_dotenv()
 
 
 def upload_dataframe_to_google_spread_sheet(df):
-    logger.info('entering')
     credentials = json.loads(os.environ.get("GCLOUD_CREDENTIALS"))
-    logger.info(credentials)
-    try:
-        gc = gspread.service_account_from_dict(credentials)
-        logger.info(gc)
-        sh = gc.open_by_key(os.environ.get("SPREADSHEET_KEY"))
-        logger.info(sh)
-        worksheet = sh.get_worksheet(0)
-        logger.info(worksheet)
-        worksheet.clear()
-        set_with_dataframe(worksheet, df)
-    except Exception as e:
-        logger.info(e)
-        logger.info('Error') 
-    
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open_by_key(os.environ.get("SPREADSHEET_KEY"))
+    worksheet = sh.get_worksheet(0)
+    worksheet.clear()
+    set_with_dataframe(worksheet, df)
     
