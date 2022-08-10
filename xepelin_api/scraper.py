c09b72d04fd93577c7ffc556a57a362e47615fc1
pirioja <pirioja@uc.cl>
2022-08-10 15:38:24 -0400

    fix: Added missing import on scraper

--- xepelin_api/scraper.py
+++ xepelin_api/scraper.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 import os
+import gspread
+from gspread_dataframe import set_with_dataframe
+
 
 def blog_scraper(url):
     chrome_options = webdriver.ChromeOptions()
@@ -33,6 +36,7 @@ def blog_scraper(url):
         posts_list.append(post_dict)
 
     df = pd.DataFrame(posts_list)
+    print(df)
     credentials = { 
     "type": os.getenv("GSPREAD_TYPE"),
     "project_id": os.getenv("GSPREAD_PROJECT_ID"),
@@ -45,7 +49,4 @@ def blog_scraper(url):
     "auth_provider_x509_cert_url": os.getenv("GSPREAD_AUTH_PROVIDER_X509_CERT_URL"),
     "client_x509_cert_url": os.getenv("GSPREAD_CLIENT_X509_CERT_URL")}
     gc = gspread.service_account_from_dict(credentials)
-    sh = gc.open_by_key(os.getenv("SPREADSHEET_KEY"))
-    worksheet = sh.get_worksheet(0)
-    worksheet.clear()
-    set_with_dataframe(worksheet, df)
+    print(gc)
