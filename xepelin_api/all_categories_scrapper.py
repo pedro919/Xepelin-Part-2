5358ee5cea5ebfe4a0b3bc8a71bb292e3401687c
pirioja <pirioja@uc.cl>
2022-08-16 10:30:26 -0400

    debug: debug for v2 scrapper

--- xepelin_api/all_categories_scrapper.py
+++ xepelin_api/all_categories_scrapper.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import exceptions
-from xepelin_api.gspread_uploader import upload_posts_to_google_spread_sheet
 import os
 
 load_dotenv()
--- xepelin_api/api.py
+++ xepelin_api/api.py
@@ -7,6 +7,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from loguru import logger
 
 load_dotenv()
 
@@ -47,11 +48,11 @@ def all_categories_scrapper_endpoint_v1(request, data: AllCategoriesInputSchema)
 
 @api.post("/all_categories_scrapper_v2", response={200: MessageSchema, 422: MessageSchema})
 def all_categories_scrapper_endpoint_v2(request, data: AllCategoriesInputSchema):
-    all_posts_list = all_categories_scrapper_function()
-    upload_posts_to_google_spread_sheet(all_posts_list)
-    try: 
-        response = requests.post(data.webhook, json={"link": os.environ.get("GSPREAD_LINK"), "email": os.environ.get("EMAIL")})
+    try:
+        all_posts_list = all_categories_scrapper_function()
+        logger.info("PASANDO A LA SUBIR LOS ARCHIVOS")
+        logger.info(all_posts_list)
+        upload_posts_to_google_spread_sheet(all_posts_list)
     except Exception as e:
-        return 200, MessageSchema(msg="Scrapper executed but unable to notify")    
-    else:
-        return 200, MessageSchema(msg="Scrapper executed successfully")
+        logger.info(e)
+        print(e)
